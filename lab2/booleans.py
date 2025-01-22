1da508f41ee42dfe92f686023f1d0c4bbd6c8fb1
Assel Bazhikey <a_bazhikey@kbtu.kz>
2025-01-22 10:32:16 +0500

    wer

---
+++ lab2/booleans.py
@@ -0,0 +1,14 @@
+print( 10 == 9  )
+print(5>6)
+print(10>4)
+#2
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+#3
+x = 150
+print(isinstance(x, int))
---
+++ lab2/operators.py
@@ -0,0 +1,8 @@
+print(102+534345)
+a=int(input())
+b=int(input())
+c=a+b
+if c>10 or c<10:
+    print("yes")
+else:
+    print("no")
