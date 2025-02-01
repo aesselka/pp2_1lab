e6586ca44485217de998118734e360684ae42da9
Assel Bazhikey <a_bazhikey@kbtu.kz>
2025-02-01 11:25:22 +0500

    func1

--- лaб3/functions.py
+++ лaб3/functions.py
@@ -1,61 +1,61 @@
-# #A recipe you are reading states how many grams you need for the ingredient. Unfortunately, your store only sells items in ounces. 
-# #1. Create a function to convert grams to ounces. ounces = 28.3495231 * grams
-# def gramms(gr):
-#     ounces= gr*28.3495231
-#     return ounces
-# print(gramms(100))
-# #2. Read in a Fahrenheit temperature. 
-# def faren(fr):
-#     c=(5/9)*(fr-32)
-#     return c
-# print(faren(int(input())))
-# #3. We count 35 heads and 94 legs among the chickens and rabbits
-# def count(heads,legs):
-#     rab=(legs-2*heads)//2
-#     chickens=heads-rab
-#     return rab,chickens
-# rab,chickens=count(35,94)
-# print(rab,chickens, sep=" , ")
-# #4. Write a function filter_prime which will take list of
-# def prime(n):
-#     if n < 2:  
-#         return False
-#     for i in range(2, int(n**0.5) + 1):
-#         if n % i == 0:
-#             return False
-#     return True
+#A recipe you are reading states how many grams you need for the ingredient. Unfortunately, your store only sells items in ounces. 
+#1. Create a function to convert grams to ounces. ounces = 28.3495231 * grams
+def gramms(gr):
+    ounces= gr*28.3495231
+    return ounces
+print(gramms(100))
+#2. Read in a Fahrenheit temperature. 
+def faren(fr):
+    c=(5/9)*(fr-32)
+    return c
+print(faren(int(input())))
+#3. We count 35 heads and 94 legs among the chickens and rabbits
+def count(heads,legs):
+    rab=(legs-2*heads)//2
+    chickens=heads-rab
+    return rab,chickens
+rab,chickens=count(35,94)
+print(rab,chickens, sep=" , ")
+#4. Write a function filter_prime which will take list of
+def prime(n):
+    if n < 2:  
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
-# def primerange(start, end):
-#     primes = []  
-#     for num in range(start, end + 1):
-#         if prime(num): 
-#             primes.append(num)
-#     return primes
-# def filter_prime(numbers):
-#     primes = []  
-#     for num in numbers:  
-#         if prime(num):  
-#             primes.append(num)  
-#     return primes 
-# numbers = list(map(int, input().split()))
-# primes = filter_prime(numbers)
-# print( *primes) 
-# #5. Write a function that accepts string from user and print all permutations of that string
-# def st(s):
-#     if len(s)==1:
-#         return s
-#     new=[]
-#     for i in range(len(s)):
-#         for j in st(s[:i]+s[i+1:]):
-#             new.append(s[i]+j)
-#     return new
-# print(*st(input()), sep=" , ")
-# #6. Write a function that accepts string from user, return a sentence with the words reversed. We are ready -> ready are We
-# def sy():
-#     sentence=input()
-#     word = sentence.split()
-#     return word[::-1]
-# print(*sy()) 
+def primerange(start, end):
+    primes = []  
+    for num in range(start, end + 1):
+        if prime(num): 
+            primes.append(num)
+    return primes
+def filter_prime(numbers):
+    primes = []  
+    for num in numbers:  
+        if prime(num):  
+            primes.append(num)  
+    return primes 
+numbers = list(map(int, input().split()))
+primes = filter_prime(numbers)
+print( *primes) 
+#5. Write a function that accepts string from user and print all permutations of that string
+def st(s):
+    if len(s)==1:
+        return s
+    new=[]
+    for i in range(len(s)):
+        for j in st(s[:i]+s[i+1:]):
+            new.append(s[i]+j)
+    return new
+print(*st(input()), sep=" , ")
+#6. Write a function that accepts string from user, return a sentence with the words reversed. We are ready -> ready are We
+def sy():
+    sentence=input()
+    word = sentence.split()
+    return word[::-1]
+print(*sy()) 
 #7 Given a list of ints, return True if the array contains a 3 next to a 3 somewhere.
 def rt(lew = []):
     for i in range(0,len(lew)):
