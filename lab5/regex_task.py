172310c7cb3f28e048b0ef7efda086d962282024
Assel Bazhikey <a_bazhikey@kbtu.kz>
2025-02-19 13:31:36 +0500

    regg

--- lab5/regex_task.py
+++ lab5/regex_task.py
@@ -19,4 +19,25 @@ for i in text3:
     for j in m:
         print(j)
 #4
-
+test1=["Anel,Dariya,Assel,bek,serdar,nurasyl"]
+for i in test1:
+    mk=re.findall(r'\b[A-Z]+[a-z]*',i )
+    for j in mk:
+        print(j)
+#5
+text1=["adcwsderb", "abdhcb","sdcb","ancrjt"]
+for i in text1:
+    if re.fullmatch(r'^a.*b$', i):
+        print(str(i)+" is correct")
+    else:
+        print(str(i)+" is not correct")
+#6
+reg=r'[., ]+'
+text2="После того как Анел закончила занятия в университете.она решила пойти в спортзал потому что почувствовала, что устала от сидячего"
+replacement=':'
+new=re.sub(reg, replacement, text2)
+print(new)
+#8
+text="JackDesk"
+mkf=(re.sub(r'([a-z])([A-Z])', r'\1 \2', text))
+print(mkf)
