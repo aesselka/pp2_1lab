d4db33cb622f4fe0e04cf8a39d06702e5d6bc214
Assel Bazhikey <a_bazhikey@kbtu.kz>
2025-03-03 08:38:00 +0500

    dirrr

--- лаб6/dir-and-files.py
+++ лаб6/dir-and-files.py
@@ -1,63 +1,71 @@
 import os
+
 #task 1
 def list_content(path):
     all_items = os.listdir(path)
 
-    print(os.path.exists(path))
+    print(os.path.isdir(os.path.join(path)))
 
     directories = [item for item in all_items if os.path.isdir(os.path.join(path, item))]
     files = [item for item in all_items if os.path.isfile(os.path.join(path, item))]
 
-    print("all its: ", all_items)
-    print("dirs: ", directories)
+    print("all items: ", all_items)
+    print("directories: ", directories)
     print("files: ", files)
 
 path = "."
 list_content(path)
-#task2
-str = "hrlli vorld"
-appr = 0
-lowr = 0
-for i in str:
-    if i.isupper():
-        appr += 1
-    elif i.islower and i != " ":
-        lowr += 1
 
-print(appr, lowr)
-#task3
-def is_palindrome(strr):
-    strr = strr.replace(" ", "").lower()
-    if str == str[::-1]:
-        print(f'"{strr}" palindrome.')
-        return
+#task 2
+print('Exist:', os.access(path, os.F_OK))
+print('Readable:', os.access(path, os.R_OK))
+print('Writable:', os.access(path, os.W_OK))
+print('Executable:', os.access(path, os.X_OK))
+#task 3
+path = '/Users/aesselka/Desktop/VSCODE'
+if os.access(path, os.F_OK):
+    filename = os.path.basename(path)
+    print(filename)
+else:
+    print('Exist:', os.access(path, os.F_OK))
+#task 4
+cnt = 0
+with open('лаб6/A.txt', 'r', encoding='utf-8') as f:
+    for _ in f:
+        cnt+=1
+
+print(cnt)
+
+#task 5
+lst = [1,2,3,4,5,6,7,8]
+with open('newfile.txt', 'w') as f:
+    for i in lst:
+        f.write(str(i) + '\n')
+
+#task 6
+import os
+os.makedirs('txts', exist_ok=True)
+for i in range(65, 91):
+    s = chr(i)
+    file_path = f"лаб6/{s}.txt"
+
+    try:
+        with open(file_path, 'x') as f:
+            print(f"file {file_path} created!")
+    except FileExistsError:
+        print(f"File {file_path} already exists!")
+
+#task 7
+with open('лаб6/A.txt', 'r') as rd, open('newfile.txt', 'w') as wt:
+    wt.write(rd.read())
+
+
+#task 8
+df = 'лаб6/text.txt'
+if os.path.exists(df):
+    if os.access(df, os.F_OK):
+        os.remove(df)
     else:
-        print(f'"{strr}" not palindrome.')
-        return
-
-str1 = "lvl"
-is_palindrome(str1)
-
-str2 = "bye"
-is_palindrome(str2)
-#task
-import time
-
-chislo = 25100
-zaderzhka = 2123
-
-time.sleep(zaderzhka / 1000)  
-
-res = chislo ** 0.5
-
-print(f"Square root of {chislo} after {zaderzhka} milliseconds is {res}")
-
-#task
-def isTrue(tup):
-    return all(tup)
-
-tup1 = (True, True, True)
-tup2 = (True, False, True)
-
-print(isTrue(tup1))
-print(isTrue(tup2))
+        print("Permision denied")
+else:
+    print("does not exist")
